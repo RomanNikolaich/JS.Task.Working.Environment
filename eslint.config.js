7bb5f0028a21e8e544a3cd0d4465f87de2d0c205
Roman <r@mail.ru>
2025-11-04 12:59:54 +0300

    second

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        es2021: true,
+        node: true
+      }
+    },
+    rules: {
+    }
+  }
+];
